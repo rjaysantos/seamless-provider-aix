983541fabfa05e18e538f92bc67427b1e128aaf1
Jor Programmer <rogelio.santos@goldenarrowsolution.com>
2025-05-09 09:38:47 +0800

    create play api

--- AixResponse.php
+++ AixResponse.php
@@ -1,7 +1,18 @@
 <?php
+
 namespace Providers\Aix;
+
+use Illuminate\Http\JsonResponse;
 
 class AixResponse
 {
-    
+    public function casinoSuccess(string $url): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'data' => $url,
+            'error' => null
+        ]);
+    }
 }
--- AixService.php
+++ AixService.php
@@ -1,7 +1,31 @@
 <?php
+
 namespace Providers\Aix;
+
+use App\Contracts\V2\IWallet;
+use App\Exceptions\Casino\WalletErrorException;
+use Illuminate\Http\Request;
 
 class AixService
 {
-    
+    public function __construct(
+        private AixRepository $repository,
+        private AixCredentials $credentials,
+        private IWallet $wallet,
+        private AixApi $api
+    ) {}
+
+    public function getLaunchUrl(Request $request): string
+    {
+        $this->repository->createIgnorePlayer(playID: $request->playId, currency: $request->currency);
+
+        $credentials = $this->credentials->getCredentialsByCurrency($request->currency);
+
+        $walletResponse = $this->wallet->Balance(credentials: $credentials, playID: $request->playId);
+
+        if ($walletResponse['status_code'] != 2100)
+            throw new WalletErrorException;
+
+        return $this->api->auth(credentials: $credentials, request: $request, balance: $walletResponse['credit']);
+    }
 }
